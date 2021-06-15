da372fad89d2479bd15c7a7b545497ea54d67073
Austin <austin362667@gmail.com>
2021-06-16 01:08:01 +0800

    init

--- src/server.js
+++ src/server.js
@@ -5,6 +5,15 @@ const userRouter = require('./routers/users.js');
 const requestLogger = require('./middleware/request-logger.js');
 const errorHandler = require('./middleware/error-handler.js');
 
+const fs = require('fs')
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const userModel = require('./model/user.js');
+const { uploadFile, getFileStream } = require('./s3')
+
 const app = express();
 
 app.use(requestLogger); // debug only
@@ -21,3 +30,31 @@ const port = 80;
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}...`);
 });
+
+app.get('/images/:key', (req, res) => {
+    console.log(req.params)
+    const key = req.params.key
+    const readStream = getFileStream(key)
+  
+    readStream.pipe(res)
+  })
+  
+  app.post('/images', upload.single('image'), async (req, res) => {
+    const file = req.file
+    console.log(file)
+  
+    // apply filter
+    // resize 
+  
+    const result = await uploadFile(file)
+    await unlinkFile(file.path)
+    console.log(result)
+    // const description = req.body.description
+    userModel
+    .update_photo('5a4b2e10-aca5-46e6-b82d-9b890b0d660f', returnData['url'])
+      .then((user) => {
+      });
+
+    res.send({imagePath: `/images/${result.Key}`})
+  })
+
